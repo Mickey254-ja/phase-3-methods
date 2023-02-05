ff39b5a7659bbc08ec620cc6b5b6bb7b74c85c67
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-02-05 17:11:54 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+greet
+
+greet "Jimmy"
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+    puts "Hello, programmer!"
+end
+greet_with_default 
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+sum = add(2,2)
+
+def halve(number)
+    return nil unless number.is_a?(Integer)
+    
+    number / 2
+  end
+
+
+
+
+
+
